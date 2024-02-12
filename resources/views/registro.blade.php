f01be198222eefe9397dfdb9478a669d6cf4baba
axlkun <axelcruz.dev@gmail.com>
2024-02-12 00:42:25 -0600

    add crsf

--- resources/views/registro.blade.php
+++ resources/views/registro.blade.php
@@ -1,30 +1,44 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registro</title>
 </head>
+
 <body>
     <h1>Registro</h1>
 
+    @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
+
     <form class="formulario" method="POST" enctype="multipart/form-data" action="{{route('validar-registro')}}">
-            <fieldset>
-                <legend>Credenciales</legend>
+        @csrf
+        <fieldset>
+            <legend>Credenciales</legend>
 
-                <label for="email">Nombre:</label>
-                <input id="nombre" name="nombre" placeholder="Ingresa tu nombre" required>
+            <label for="name">Nombre:</label>
+            <input id="name" name="name" placeholder="Ingresa tu nombre" value="{{ old('name') }}" required>
 
-                <label for="email">E-mail:</label>
-                <input type="email" id="email" name="email" placeholder="Ingresa tu correo" required>
+            <label for="email">E-mail:</label>
+            <input type="email" id="email" name="email" placeholder="Ingresa tu correo" value="{{ old('email') }}" required>
 
-                <label for="contraseña">Contraseña:</label>
-                <input type="password" id="contraseña" name="password" placeholder="Ingresa tu contraseña" required>
-            </fieldset>
+            <label for="contraseña">Contraseña:</label>
+            <input type="password" id="contraseña" name="password" placeholder="Ingresa tu contraseña" value="{{ old('password') }}" required>
+        </fieldset>
 
-            <button type="submit">Registrarse</button>
+        <button type="submit">Registrarse</button>
 
-        </form>
+    </form>
 
 </body>
+
 </html>
