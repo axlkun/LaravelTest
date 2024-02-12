9be12c8526ef36e8ac9204eb680c5c26bede90d5
axlkun <axelcruz.dev@gmail.com>
2024-02-12 00:42:42 -0600

    add logout function

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,8 +3,43 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-    //
+    public function register(Request $request){
+        $validated = $request->validate([
+            'email' => 'required|unique:users|email',
+            'name' => 'required',
+            'password' => 'required|min:7'
+        ]);
+
+        $user = new User();
+
+        $user->name = $validated['name'];
+        $user->email = $validated['email'];
+        $user->password = Hash::make($validated['password']);
+
+        $user->save();
+
+        Auth::login($user);
+
+        return redirect(route('privada'))->withInput();
+    }
+
+    public function login(Request $request){
+
+    }
+
+    public function logout(Request $request){
+        Auth::logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+
+    }
 }
