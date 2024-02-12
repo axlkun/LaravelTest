42e0df5075a9fea7a94f1c64f7e42fcfc00a1645
axlkun <axelcruz.dev@gmail.com>
2024-02-11 22:42:15 -0600

    add views

---
+++ resources/views/login.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <h1>Login</h1>
+
+    <form class="formulario" method="POST" enctype="multipart/form-data" action="{{route('inicia-sesion')}}">
+            <fieldset>
+                <legend>Credenciales</legend>
+
+                <label for="email">Nombre:</label>
+                <input id="nombre" name="nombre" placeholder="Ingresa tu nombre" required>
+
+                <label for="email">E-mail:</label>
+                <input type="email" id="email" name="email" placeholder="Ingresa tu correo" required>
+
+                <label for="contraseña">Contraseña:</label>
+                <input type="password" id="contraseña" name="password" placeholder="Ingresa tu contraseña" required>
+            </fieldset>
+
+            <button type="submit">Registrarse</button>
+
+        </form>
+
+</body>
+</html>
